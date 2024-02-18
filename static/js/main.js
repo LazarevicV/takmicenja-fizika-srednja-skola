d21a2e29ca9c3c2b335c177e5c361802277633f3
fehimb996 <kingring2009@live.com>
2024-02-18 14:11:02 +0100

    manje izmene oko izgleda

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM učitan.");
+
+    // Koristite url_for kako biste dobili URL trenutne stranice
+    var currentUrl = "{{ url_for('index') }}"; // Zamenite 'index' sa imenom vašeg view-a
+    console.log("Trenutni URL:", currentUrl);
+
+    var currentLink = document.getElementById(currentUrl + '-link');
+    console.log("Pronađeni link:", currentLink);
+
+    if (currentLink) {
+        currentLink.classList.add('active');
+        console.log("Active klasa dodata.");
+    }
+});
+
 let mybutton = document.getElementById("btn-back-to-top");
 
 window.onscroll = function () {
