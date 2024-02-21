675312468be97250d39703f6935593977c51282c
fehimb996 <kingring2009@live.com>
2024-02-21 01:32:03 +0100

    vesti2 WIP

--- static/js/main.js
+++ static/js/main.js
@@ -36,3 +36,41 @@ function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const newsPages = document.querySelectorAll('.news-page');
+    let currentPage = 1;
+
+    function showPage(pageNumber) {
+        newsPages.forEach(page => {
+            page.style.display = "none";
+        });
+
+        const selectedPage = document.querySelector(`.news-page[data-page="${pageNumber}"]`);
+        if (selectedPage) {
+            selectedPage.style.display = "flex"; // Podešavanje stila na flex ako koristite Bootstrap grid system
+        }
+    }
+
+    function updatePagination() {
+        const paginationLinks = document.querySelectorAll('.pagination .page-link');
+        paginationLinks.forEach(link => {
+            link.addEventListener('click', function (event) {
+                event.preventDefault();
+                const targetPage = this.innerText;
+                if (targetPage === '«') {
+                    currentPage = Math.max(1, currentPage - 1);
+                } else if (targetPage === '»') {
+                    currentPage = Math.min(newsPages.length, currentPage + 1);
+                } else {
+                    currentPage = parseInt(targetPage);
+                }
+
+                showPage(currentPage);
+            });
+        });
+    }
+
+    showPage(currentPage);
+    updatePagination();
+});
