aed78b1a122362743525ecd8f04ef65464e3d240
fehimb996 <kingring2009@live.com>
2024-03-06 20:01:57 +0100

    zavrsena skripta za okruzno i drzavno zadaci

--- main.py
+++ main.py
@@ -136,9 +136,9 @@ def opstinsko_zadaci():
     ostalo_opstinsko = []
 
     for o in opstinsko_fajlovi:
-        if 'BETA' in o['name'] or 'beta' in o['name']:
+        if 'BETA' in o['name'] or 'beta' in o['name'] or 'Beta' in o['name']:
             beta_opstinsko.append(o)
-        elif 'ALFA' in o['name'] or 'alfa' in o['name']:
+        elif 'ALFA' in o['name'] or 'alfa' in o['name'] or 'Alfa' in o['name']:
             alfa_opstinsko.append(o)
         else:
             ostalo_opstinsko.append(o)
@@ -161,70 +161,76 @@ def drzavno_zadaci():
     godine = []
     for o in drzavno_fajlovi:
         trenutno = o['name'].split('_')
-        if (len(trenutno[0])>2 and trenutno[0] not in godine): #or (len(trenutno[1])>2 and trenutno[1] not in godine):
+        if (len(trenutno[0]) > 2 and trenutno[0] not in godine): 
             godine.append(trenutno[0])
-        elif (len(trenutno[1])>2 and trenutno[1] not in godine):
+        elif (len(trenutno[1]) > 2 and trenutno[1] not in godine):
             godine.append(trenutno[1])
     godine.sort(reverse=True)
     prave_godine = []
 
     alfa_drzavno = []
     beta_drzavno = []
+    gama_drzavno = []  # Dodajte gama kategoriju
     ostalo_drzavno = []
 
     for o in drzavno_fajlovi:
-        if 'BETA' in o['name'] or 'beta' in o['name']:
+        if 'BETA' in o['name'] or 'beta' in o['name'] or 'Beta' in o['name']:
             beta_drzavno.append(o)
-        elif 'ALFA' in o['name'] or 'alfa' in o['name']:
+        elif 'ALFA' in o['name'] or 'alfa' in o['name'] or 'Alfa' in o['name']:
             alfa_drzavno.append(o)
         else:
             ostalo_drzavno.append(o)
+
+    for gama_file in gama_fajlovi:
+        gama_drzavno.append(gama_file)
 
     for i in range(6):
         prave_godine.append(godine[i])
 
     alfa_drzavno = sorted(alfa_drzavno, key=lambda x: x['name'])
     beta_drzavno = sorted(beta_drzavno, key=lambda x: x['name'])
+    gama_drzavno = sorted(gama_drzavno, key=lambda x: x['name'])
     ostalo_drzavno = sorted(ostalo_drzavno, key=lambda x: x['name'])
-    
-    
-            
-    return render_template('drzavno_zadaci.html', godine=prave_godine, opstinsko=opstinsko_fajlovi, alfa=alfa_drzavno, beta=beta_drzavno, ostalo = ostalo_drzavno)
+
+    return render_template('drzavno_zadaci.html', godine=prave_godine, alfa=alfa_drzavno, beta=beta_drzavno, gama=gama_drzavno, ostalo=ostalo_drzavno)
 
 @app.route('/okruzno_zadaci')
 def okruzno_zadaci():
     godine = []
     for o in okruzno_fajlovi:
         trenutno = o['name'].split('_')
-        if (len(trenutno[0])>2 and trenutno[0] not in godine): #or (len(trenutno[1])>2 and trenutno[1] not in godine):
+        if (len(trenutno[0]) > 2 and trenutno[0] not in godine): 
             godine.append(trenutno[0])
-        elif (len(trenutno[1])>2 and trenutno[1] not in godine):
+        elif (len(trenutno[1]) > 2 and trenutno[1] not in godine):
             godine.append(trenutno[1])
     godine.sort(reverse=True)
     prave_godine = []
 
     alfa_okruzno = []
     beta_okruzno = []
+    gama_okruzno = []  # Dodajte gama kategoriju
     ostalo_okruzno = []
 
     for o in okruzno_fajlovi:
-        if 'BETA' in o['name'] or 'beta' in o['name']:
+        if 'BETA' in o['name'] or 'beta' in o['name'] or 'Beta' in o['name']:
             beta_okruzno.append(o)
-        elif 'ALFA' in o['name'] or 'alfa' in o['name']:
+        elif 'ALFA' in o['name'] or 'alfa' in o['name'] or 'Alfa' in o['name']:
             alfa_okruzno.append(o)
         else:
             ostalo_okruzno.append(o)
+
+    for gama_file in gama_fajlovi:
+        gama_okruzno.append(gama_file)
 
     for i in range(6):
         prave_godine.append(godine[i])
 
     alfa_okruzno = sorted(alfa_okruzno, key=lambda x: x['name'])
     beta_okruzno = sorted(beta_okruzno, key=lambda x: x['name'])
+    gama_okruzno = sorted(gama_okruzno, key=lambda x: x['name'])
     ostalo_okruzno = sorted(ostalo_okruzno, key=lambda x: x['name'])
-    
-    
-            
-    return render_template('okruzno_zadaci.html', godine=godine, opstinsko=opstinsko_fajlovi, alfa=alfa_okruzno, beta=beta_okruzno, ostalo = ostalo_okruzno)
+
+    return render_template('okruzno_zadaci.html', godine=prave_godine, alfa=alfa_okruzno, beta=beta_okruzno, gama=gama_okruzno, ostalo=ostalo_okruzno)
 
 # print(opstinsko_fajlovi)
 # print('*' *50)
